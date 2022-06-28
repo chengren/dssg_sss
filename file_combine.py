d891e36c2d9d8638f1553cf72b5130fa52e86f90
chengren <cnzjbob@gmail.com>
2022-06-28 14:41:14 -0700

    fix columns without by family
    If meet columns without'by family", still make it running and return the file name

--- file_combine.py
+++ file_combine.py
@@ -7,19 +7,22 @@ def combine_table(excel_files):
 	n_col_max = 0
 	for file in excel_files:
 		# read in excel as pandas dataframe
-		df = pd.read_excel(file, sheet_name = 'By Family')
-		#count # of columns
-		n_col = df.shape[1]
-		if n_col > n_col_max:
-			n_col_max = n_col
-		# call the packages from preprocess
-		df = perprocess.std_col_names (df)
-		#combine excel file
-		df_combine = pd.concat([df_combine,df],axis=0)
-		#NOTE: if the columns names are the same, they will connnect. Otherwiese creating new column
-	n_col_combine = df_combine.shape[1]
-	if n_col_combine > n_col_max:
-		print('There might be some typos in columns names')
+		try:
+			df = pd.read_excel(file, sheet_name = 'By Family')
+			#count # of columns
+			n_col = df.shape[1]
+			if n_col > n_col_max:
+				n_col_max = n_col
+			# call the packages from preprocess
+			df = perprocess.std_col_names (df)
+			#combine excel file
+			df_combine = pd.concat([df_combine,df],axis=0)
+			#NOTE: if the columns names are the same, they will connnect. Otherwiese creating new column
+			n_col_combine = df_combine.shape[1]
+			if n_col_combine > n_col_max:
+				print('There might be some typos in columns names')
+		except:
+			print(file)
 	return df_combine
 
 	#BUG Warning: Typo in preshooler in WA2021_SSS_Partial.xlsb
