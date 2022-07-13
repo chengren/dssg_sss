1743e08c73f20f4aa4db20acbda8f880ad89117d
chengren <cnzjbob@gmail.com>
2022-07-13 16:11:09 -0700

    Update and rename csv_sql.py to combine_to_csv.py

--- combine_to_csv.py
+++ combine_to_csv.py
@@ -5,16 +5,17 @@ import perprocess
 import file_combine
 
 # create sqlalchemy db connetion
-engine = create_engine('sqlite:///dssg.sqlite', echo=False)
-sqlite_connection = engine.connect()
+#engine = create_engine('sqlite:///dssg.sqlite', echo=False)
+#sqlite_connection = engine.connect()
 
 # get current working directory path
 # get list of files in current working directory
 path = os.getcwd()+'/data/'
 xls_files = glob.glob(os.path.join(path, "*.xls*"))
 df_combine = file_combine.combine_table(xls_files)
+df_combine.to_csv('df_combine_sss.csv',index=False)
 # write dataframe to sqlite, overwrite table if it exists
-df_combine.to_sql('SSS', sqlite_connection, if_exists='replace')
+# df_combine.to_sql('SSS', sqlite_connection, if_exists='replace')
 
 # after the process completes, closes the databasse connection
-sqlite_connection.close()
+#sqlite_connection.close()
